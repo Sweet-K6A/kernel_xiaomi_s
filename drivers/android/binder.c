49d889c35d1fa47e9c13f8451551c9fcc638f873
Marco Ballesio <balejs@google.com>
2020-08-18 18:42:58 -0700

    binder: implement BINDER_FREEZE ioctl
    Frozen tasks can't process binder transactions, so a way is required to
    inform transmitting ends of communication failures due to the frozen
    state of their receiving counterparts. Additionally, races are possible
    between transitions to frozen state and binder transactions enqueued to a
    specific process.
    
    Implement BINDER_FREEZE ioctl for user space to inform the binder driver
    about the intention to freeze or unfreeze a process. When the ioctl is
    called, block the caller until any pending binder transactions toward the
    target process are flushed. Return an error to transactions to processes
    marked as frozen.
    
    Bug: 143717177
    Change-Id: Ie16f72b490bbe1785b82dee2442452f71ad7dc65
    Signed-off-by: Marco Ballesio <balejs@google.com>
    Co-developed-by: Todd Kjos <tkjos@google.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Shakib <its.shakib@proton.me>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -498,6 +498,8 @@ struct binder_priority {
  *                        bit 1: new pending sync transaction during freezing
  *                        (protected by @inner_lock)
  * @async_recv:           process received async transactions since last frozen
+ * @is_frozen:            process is frozen and unable to service
+ *                        binder transactions
  *                        (protected by @inner_lock)
  * @freeze_wait:          waitqueue of processes waiting for all outstanding
  *                        transactions to be processed
@@ -2993,6 +2995,7 @@ static int binder_proc_transaction(struct binder_transaction *t,
 
 	if ((proc->is_frozen && !oneway) || proc->is_dead ||
 			(thread && thread->is_dead)) {
+	if (proc->is_frozen || proc->is_dead || (thread && thread->is_dead)) {
 		bool proc_is_dead = proc->is_dead
 			|| (thread && thread->is_dead);
 		binder_inner_proc_unlock(proc);
@@ -3647,6 +3650,9 @@ static void binder_transaction(struct binder_proc *proc,
 		binder_inner_proc_lock(target_proc);
 		if (target_thread->is_dead) {
 			return_error = BR_DEAD_REPLY;
+		if (target_thread->is_dead || target_proc->is_frozen) {
+			return_error = target_proc->is_dead ?
+				BR_DEAD_REPLY : BR_FROZEN_REPLY;
 			binder_inner_proc_unlock(target_proc);
 			goto err_dead_proc_or_thread;
 		}
@@ -5341,6 +5347,8 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		int target_procs_count = 0, i = 0;
 
 		ret = 0;
+		struct binder_proc *target_proc;
+		bool found = false;
 
 		if (copy_from_user(&info, ubuf, sizeof(info))) {
 			ret = -EFAULT;
@@ -5391,6 +5399,50 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		kfree(target_procs);
 
+		mutex_lock(&binder_procs_lock);
+		hlist_for_each_entry(target_proc, &binder_procs, proc_node) {
+			if (target_proc->pid == info.pid) {
+				found = true;
+				binder_inner_proc_lock(target_proc);
+				atomic_inc(&target_proc->tmp_ref);
+				binder_inner_proc_unlock(target_proc);
+				break;
+			}
+		}
+		mutex_unlock(&binder_procs_lock);
+		if (!found) {
+			ret = -EINVAL;
+			goto err;
+		}
+		if (!info.enable) {
+			binder_inner_proc_lock(target_proc);
+			target_proc->is_frozen = false;
+			binder_inner_proc_unlock(target_proc);
+			binder_proc_dec_tmpref(target_proc);
+			break;
+		}
+		/*
+		 * Freezing the target. Prevent new transactions by
+		 * setting frozen state. If timeout specified, wait
+		 * for transactions to drain.
+		 */
+		binder_inner_proc_lock(target_proc);
+		target_proc->is_frozen = true;
+		binder_inner_proc_unlock(target_proc);
+		if (info.timeout_ms > 0)
+			ret = wait_event_interruptible_timeout(
+				target_proc->freeze_wait,
+				(!target_proc->outstanding_txns),
+				msecs_to_jiffies(info.timeout_ms));
+		if (!ret && target_proc->outstanding_txns) {
+			ret = -EAGAIN;
+		}
+		if (ret < 0) {
+			binder_inner_proc_lock(target_proc);
+			target_proc->is_frozen = false;
+			binder_inner_proc_unlock(target_proc);
+		}
+		binder_proc_dec_tmpref(target_proc);
 		if (ret < 0)
 			goto err;
 		break;
