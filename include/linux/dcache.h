fbc1edf22e56600ac12eb0176182025ba5af7a2c
Yaroslav Furman <yaro330@gmail.com>
2020-08-23 00:17:41 +0300

    dcache: increase DNAME_INLINE_LEN on ARM64 to 96 bytes
    This ensures that most dname allocations in __d_alloc come from
    kmem_cache allocated struct dentry, thus improving performance.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Shakib <its.shakib@proton.me>

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -78,6 +78,7 @@ extern struct dentry_stat_t dentry_stat;
 #ifdef CONFIG_64BIT
 #ifdef CONFIG_ARM64
 # define DNAME_INLINE_LEN 32 + 192 /* 384 bytes */
+# define DNAME_INLINE_LEN 96 /* 256 bytes */
 #else
 # define DNAME_INLINE_LEN 32 /* 192 bytes */
 #endif
