59a2b1ece92a6c0027dfe299071a59563521166f
Danny Lin <danny@kdrag0n.dev>
2021-11-21 00:47:31 +0530

    msm: kgsl: Convert from nr_indirectly_reclaimable_bytes to nr_kernel_misc_reclaimable
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Shakib <its.shakib@proton.me>

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -112,7 +112,7 @@ _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 	atomic_inc(&pool->page_count);
 
 	mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
-				(1 << pool->pool_order));
+				pool->pool_order);
 }
 
 /* Returns a page from specified pool */
@@ -133,6 +133,7 @@ _kgsl_pool_get_page(struct kgsl_page_pool *pool)
 				NR_KERNEL_MISC_RECLAIMABLE,
 				-(1 << pool->pool_order));
 	}
+				-pool->pool_order);
 	return p;
 }
 
