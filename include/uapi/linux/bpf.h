d5d996b7e619ac0e723a97f35403d5ed02bec595
Shakib <its.shakib@proton.me>
2024-12-17 07:09:12 +0000

    bpf: Add support for `Android V qpr1`
    - Brought up commits from here :
    * commit 1 : https://github.com/LineageOS/android_kernel_qcom_sm8150/commit/6b3a3a549ab1d380871b2b733b80f5908e04becc
    * commit 2 : https://github.com/LineageOS/android_kernel_qcom_sm8150/commit/0838aa85b7dfa17c92f088c63ac09c66f5ed655a
    * commit 3 : https://github.com/LineageOS/android_kernel_qcom_sm8150/commit/81c6b83d85bf98856b8069075e9e73d661fbe73e
    * commit 4 : https://github.com/LineageOS/android_kernel_qcom_sm8150/commit/5c69e70322f52300f2869759c169e85bcabcc5b9
    
    Signed-off-by: Shakib <its.shakib@proton.me>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -251,7 +251,7 @@ union bpf_attr {
 		__u32	numa_node;	/* numa node (effective only if
 					 * BPF_F_NUMA_NODE is set).
 					 */
-		char	map_name[BPF_OBJ_NAME_LEN];
+		__u8	map_name[BPF_OBJ_NAME_LEN];
 	};
 
 	struct { /* anonymous struct used by BPF_MAP_*_ELEM commands */
@@ -274,13 +274,7 @@ union bpf_attr {
 		__aligned_u64	log_buf;	/* user supplied buffer */
 		__u32		kern_version;	/* checked when prog_type=kprobe */
 		__u32		prog_flags;
-		char		prog_name[BPF_OBJ_NAME_LEN];
-		__u32		prog_ifindex;	/* ifindex of netdev to prep for */
-		/* For some prog types expected attach type must be known at
-		 * load time to verify attach type specific parts of prog
-		 * (context accesses, allowed helpers, etc).
-		 */
-		__u32		expected_attach_type;
+		__u8		prog_name[BPF_OBJ_NAME_LEN];
 	};
 
 	struct { /* anonymous struct used by BPF_OBJ_* commands */
@@ -1012,7 +1006,7 @@ struct bpf_prog_info {
 	__u32 created_by_uid;
 	__u32 nr_map_ids;
 	__aligned_u64 map_ids;
-	char name[BPF_OBJ_NAME_LEN];
+	__u8 name[BPF_OBJ_NAME_LEN];
 } __attribute__((aligned(8)));
 
 struct bpf_map_info {
@@ -1022,7 +1016,7 @@ struct bpf_map_info {
 	__u32 value_size;
 	__u32 max_entries;
 	__u32 map_flags;
-	char  name[BPF_OBJ_NAME_LEN];
+	__u8  name[BPF_OBJ_NAME_LEN];
 } __attribute__((aligned(8)));
 
 /* User bpf_sock_addr struct to access socket fields and sockaddr struct passed
